f8fdcd69e457deda9b501b41cae8d9b164f316e2
Geovanni Corsino <geovannicorsino@gmail.com>
2021-09-19 22:39:39 -0300

    feature(hello): apiview and serializer

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from . import serializers
+
 
 # Create your views here.
+
+
+class HelloApiView(views.APIView):
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'It is similar to a traditional Django view',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
