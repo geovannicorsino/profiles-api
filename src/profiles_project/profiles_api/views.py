668a301f4c773aa82c8def546cb36c43df2b0e87
Geovanni Corsino <geovannicorsino@gmail.com>
2021-09-22 21:10:33 -0300

    feat: add permissions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import views
-from rest_framework import viewsets, status
+from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
 from . import models
+from . import permissions
 
 
 # Create your views here.
@@ -79,3 +82,5 @@ class HelloViewSet(viewsets.ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
